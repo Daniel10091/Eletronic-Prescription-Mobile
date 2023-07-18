85d8b7a02bf016dae29af10f156228688a27e859
Daniel Aguiar <dc03934@gmail.com>
2023-07-18 11:49:17 -0300

    Project base created

---
+++ eletronic-prescription/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'eletronic.prescription',
+  appName: "'eletronic-prescription'",
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
